4ceacb8d8b473507ce3e44a556e30e26c066e334
giacomo096 <14-11085@usb.ve>
2022-10-26 20:41:51 -0400

    cambios en la salida y renombre de archivos.py

---
+++ func_lexer.py
@@ -0,0 +1,119 @@
+import ply.lex as lex
+
+# Debe ignorar los espacios en blanco, tabulaciones, saltos de linea,  y comentarios
+t_ignore = ' \t'
+
+# Palabras reservadas
+reserved = {
+    'skip' : 'TkSkip',
+    'if'   : 'TkIf',
+    'fi'   : 'TkFi',
+    'do'   : 'TkDo',
+    'od'   : 'TkOd',
+    'for'  : 'TkFor',
+    'rof'  : 'TkRof',
+    'int'  : 'TkInt',
+    'true' : 'TkTrue',
+    'false': 'TkFalse',
+    'print': 'TkPrint',
+    'declare': 'TkDeclare',
+    'array': 'TkArray',
+    'bool' : 'TkBool',
+}
+
+tokens = [
+    'TkSoForth', 'TkComma', 'TkOpenPar',
+    'TkClosePar', 'TkAsig', 'TkSemicolon', 'TkArrow', 'TkPlus',
+    'TkMinus', 'TkMult', 'TkNot', 'TkLess',
+    'TkLeq', 'TkGeg', 'TkGreater', 'TkEqual', 'TkNEqual',
+    'Tk0Bracket', 'TkCBracket', 'TkTwoPoints', 'TkConcat',
+    'TkId', 'TkNum', 'TkString', 'TkAnd', 'TkOr',
+    'Tk0Block', 'TkCBlock'
+]
+
+t_TkPlus = r'\+'
+t_TkOpenPar = r'\('
+t_TkClosePar = r'\)'
+t_TkNot = r'\!'
+t_Tk0Bracket = r'\['
+t_TkCBracket = r'\]'
+t_TkMult = r'\*'
+t_Tk0Block = r'\|\['
+t_TkCBlock = r'\]\|'
+t_TkAnd = r'/\\'
+t_TkOr = r'\\/'
+t_TkSoForth = r'\.\.'
+t_TkConcat = r'\.'
+t_TkComma = r','
+t_TkAsig = r':='
+t_TkSemicolon = r';'
+t_TkArrow = r'-->'
+t_TkMinus = r'-'
+t_TkLess = r'<'
+t_TkLeq = r'<='
+t_TkGeg = r'>='
+t_TkGreater = r'>'
+t_TkEqual = r'=='
+t_TkNEqual = r'!='
+t_TkTwoPoints = r':'
+
+tokens = tokens + list(reserved.values())
+
+def t_TkId(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'TkId')    # Check for reserved words
+    return t
+
+def t_TkNum(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_TkString(t):
+    r'\".*?\"'
+    t.value = t.value[1:-1] # remueve las comillas
+    return t
+
+# Ignora nuevas lineas
+def t_newline(t):
+    r'\n+'
+    pass
+
+# Ignora comentarios
+def t_comment(t):
+    r'//.*'
+    pass
+
+# Error handling rule
+def t_error(t):
+    print("Error: Unexpected character '%s' in column " % t.value[0],t.lexpos+1)
+    t.lexer.skip(1)
+
+# Build the lexer
+lexer = lex.lex()
+
+
+# Give the lexer some input
+def work(data,line):
+
+    lexer.input(data)
+    # Tokenize
+    while True:
+        tok = lexer.token()
+        if not tok: 
+            break
+        else:
+            if tok.type == 'TkId':
+                print(tok.type + "(\"" + tok.value + "\")", line, tok.lexpos +1)
+            elif tok.type == 'TkNum':
+                print(tok.type + "(" + str(tok.value) + ")", line, tok.lexpos +1)
+            elif tok.type == 'TkString':
+                print(tok.type + "(\"" + tok.value + "\")", line, tok.lexpos +1)
+            else:
+                print(tok.type, line, tok.lexpos +1)
+
+### Cosas que faltan: ###
+# - Detectar errores
+# - Ver si hay más palabras reservadas 
+# - Crear función main donde se pida el input que va a pasar a través del lexer (liiiisto)
+# - Imprimir fila y columna correctamente  creeeeeeo que funciona
--- lexer.py
+++ lexer.py
@@ -1,121 +1,31 @@
-import ply.lex as lex
+import sys
+import func_lexer as lexer
 
-# Debe ignorar los espacios en blanco, tabulaciones, saltos de linea,  y comentarios
-t_ignore = ' \t'
+# Funcion main, ejemplo de entrada: python main.py archivo.gcl
+# Solo se aceptan archivos .gcl
+# Se debe colocar en el archivo.gcl el codigo a ser analizado
 
-# Palabras reservadas
-reserved = {
-    'skip' : 'TkSkip',
-    'if'   : 'TkIf',
-    'fi'   : 'TkFi',
-    'do'   : 'TkDo',
-    'od'   : 'TkOd',
-    'for'  : 'TkFor',
-    'rof'  : 'TkRof',
-    'int'  : 'TkInt',
-    'true' : 'TkTrue',
-    'false': 'TkFalse',
-    'print': 'TkPrint',
-    'declare': 'TkDeclare',
-    'array': 'TkArray',
-    'bool' : 'TkBool',
-}
-
-tokens = [
-    'TkSoForth', 'TkComma', 'TkOpenPar',
-    'TkClosePar', 'TkAsig', 'TkSemicolon', 'TkArrow', 'TkPlus',
-    'TkMinus', 'TkMult', 'TkNot', 'TkLess',
-    'TkLeq', 'TkGeg', 'TkGreater', 'TkEqual', 'TkNEqual',
-    'Tk0Bracket', 'TkCBracket', 'TkTwoPoints', 'TkConcat',
-    'TkId', 'TkNum', 'TkString', 'TkAnd', 'TkOr',
-    'Tk0Block', 'TkCBlock'
-]
-
-t_TkPlus = r'\+'
-t_TkOpenPar = r'\('
-t_TkClosePar = r'\)'
-t_TkNot = r'\!'
-t_Tk0Bracket = r'\['
-t_TkCBracket = r'\]'
-t_TkMult = r'\*'
-t_Tk0Block = r'\|\['
-t_TkCBlock = r'\]\|'
-t_TkAnd = r'/\\'
-t_TkOr = r'\\/'
-t_TkSoForth = r'\.\.'
-t_TkConcat = r'\.'
-t_TkComma = r','
-t_TkAsig = r':='
-t_TkSemicolon = r';'
-t_TkArrow = r'-->'
-t_TkMinus = r'-'
-t_TkLess = r'<'
-t_TkLeq = r'<='
-t_TkGeg = r'>='
-t_TkGreater = r'>'
-t_TkEqual = r'='
-t_TkNEqual = r'!='
-t_TkTwoPoints = r':'
-
-tokens = tokens + list(reserved.values())
-
-def t_TkId(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reserved.get(t.value,'TkId')    # Check for reserved words
-    return t
-
-def t_TkNum(t):
-    r'\d+'
-    t.value = int(t.value)
-    return t
-
-def t_TkString(t):
-    r'\".*?\"'
-    t.value = t.value[1:-1] # remueve las comillas
-    return t
-
-# Ignora nuevas lineas
-def t_newline(t):
-    r'\n+'
-    pass
-
-# Ignora comentarios
-def t_comment(t):
-    r'//.*'
-    pass
-
-# Error handling rule
-def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
-    t.lexer.skip(1)
-
-# Build the lexer
-lexer = lex.lex()
+def main():
+    try:
+        filename = sys.argv[1]
+    except:
+        print("Introduzca nombre de archivo (python main.py input.gcl)")
+        return 0
+    doc = sys.argv[1].split(".")
+    if doc[-1] == "gcl":
+        file = open(filename,"r")
+        row = 1
+        try:
+            for line in file.readlines():
+                lexer.work(line,row)               
+                row += 1
+        except:
+           print("archivo no existe")
+           return 1
+    else:
+        print("arhivo solo puede ser de tipo .gcl")
+   
 
 
-### ESTO DEBERÍA IR EN UN MAIN OR SOMEHWERE ELSE###
-
-# Give the lexer some input
-def work(data):
-
-    lexer.input(data)
-    # Tokenize
-    while True:
-        tok = lexer.token()
-        if not tok: 
-            break
-        else:
-            if tok.type == 'TkId':
-                print(tok.type + "(\"" + tok.value + "\")", tok.lineno, tok.lexpos)
-            elif tok.type == 'TkNum':
-                print(tok.type + "(" + str(tok.value) + ")", tok.lineno, tok.lexpos)
-            elif tok.type == 'TkString':
-                print(tok.type + "(\"" + tok.value + "\")", tok.lineno, tok.lexpos)
-            else:
-                print(tok.type, tok.lineno, tok.lexpos)
-
-### Cosas que faltan: ###
-# - Detectar errores
-# - Ver si hay más palabras reservadas
-# - Crear función main donde se pida el input que va a pasar a través del lexer (liiiisto)
-# - Imprimir fila y columna correctamente
+if __name__ == "__main__":
+    main()
